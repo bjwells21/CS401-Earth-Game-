dac3a1842c0fc50f0fe4e351cd481e8bc6951f7b
Rohan Bierneni <rohan.bierneni@gmail.com>
2023-03-04 12:57:47 -0800

    Implemented a draw random climate card method

--- src/main/java/earthGame/Model/ClimateCard.java
+++ src/main/java/earthGame/Model/ClimateCard.java
@@ -2,6 +2,8 @@ package earthGame.Model;
 
 import earthGame.Model.Card;
 
+import java.util.Random;
+
 enum Climate {
     TROPICAL,
     DRY,
@@ -12,12 +14,35 @@ enum Climate {
 
 public class ClimateCard extends Card {
     private int victoryPoints;
+    private Climate c;
 
     public ClimateCard(){
         victoryPoints = 0;
     }
-    public ClimateCard(int victoryPoints){
-        this.victoryPoints = victoryPoints;
+    public ClimateCard(String name){
+        c = Climate.valueOf(name);
+        switch(c){
+            case TROPICAL -> {
+                victoryPoints = 1;
+            }
+            case DRY -> {
+                victoryPoints = 2;
+            }
+            case TEMPERATE -> {
+                victoryPoints = 3;
+            }
+            case CONTINENTAL -> {
+                victoryPoints = 4;
+            }
+            case POLAR -> {
+                victoryPoints = 5;
+            }
+        }
+    }
+
+    public ClimateCard randomDrawCard(){
+        Climate tmp = Climate.values()[new Random().nextInt(Climate.values().length)];
+        return new ClimateCard(tmp.name());
     }
 
     public void abilityEffect(){
@@ -30,5 +55,13 @@ public class ClimateCard extends Card {
 
     public void setVictoryPoints(int victoryPoints) {
         this.victoryPoints = victoryPoints;
+    }
+
+    public Climate getC() {
+        return c;
+    }
+
+    public void setC(Climate c) {
+        this.c = c;
     }
 }
