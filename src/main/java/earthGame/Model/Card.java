4ed29592f25ab349e3434c28c068f5646709eee4
Rohan Bierneni <rohan.bierneni@gmail.com>
2023-04-14 21:22:23 -0700

    Added block comments for functions

--- src/main/java/earthGame/Model/Card.java
+++ src/main/java/earthGame/Model/Card.java
@@ -2,7 +2,7 @@ package earthGame.Model;
 
 import java.util.ArrayList;
 
-public abstract class Card {
+public class Card {
     public String name;
     public String subName;
     public ArrayList<String> habitatElements = new ArrayList<String>();
