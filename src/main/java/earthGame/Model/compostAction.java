b90ea0c16828be624f72443d54875b15e0316ab4
Tyler D <tylinese@gmail.com>
2023-05-02 16:50:11 -0700

    added compost action

---
+++ src/main/java/earthGame/Model/compostAction.java
@@ -0,0 +1,18 @@
+package earthGame.Model;
+
+public class compostAction {
+    public void compostInit(Player player, cardDeck deck, cardDeck discard, int choice){
+        if (player.isActivePlayer()){
+            discard.cards.add(deck.drawCard());
+            player.addCompost(2);
+            player.addSoil(5);
+        } else {
+            if (choice == 1) {
+                player.addCompost(2);
+            } else {
+                player.addSoil(2);
+            }
+        }
+    }
+
+}
